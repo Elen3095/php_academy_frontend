df3facc6eb2de5c2d648ebaed8fa20fd31fb7408
Aliona Byrchenko <alena3095@ukr.net>
2017-07-25 00:03:31 +0300

    homework 6

--- 2.js
+++ 2.js
@@ -1,8 +1,37 @@
-// task2
-var x1=prompt("the first var:", 1);
-var x2=prompt("the second var:", 1);
-var x3=prompt("the third var:", 1);
+// Створіть чотири функції для арифметичних дій: додавання, віднімання,
+ // множення і ділення. Кожна функція повинна приймати 2 параметра і виводити
+ //  результат їх обчимлення. Функція ділення повинна робити перевірку ділення на нуль.
+ function sum(a,b){
+   return +a + +b;
+ }
+ function pow(a,b){
+   return a * b;
+ }
+ function sub(a,b){
+   return a - b;
+ }
+ function div(a,b){
+   if (b==0){
+     return "ділення на нуль";
+   } else{
+     return a / b;
+   }
+ }
 
-var arifm=(+x1 + +x2 + +x3)/3;
-
-alert("the rezult of operation of 2-d task="+" "+arifm);
+ var x1 = prompt('first value =',1);
+ var x2= prompt('second value =',1);
+ var func= prompt('function:(sum, pow, sub, div)', "sum");
+ switch (func) {
+   case "sum":
+       alert (sum(x1, x2));
+     break;
+  case "pow":
+       alert( pow(x1, x2)) ;
+      break;
+      case "sub":
+          alert( sub(x1, x2) );
+          break;
+          case "div":
+              alert( div(x1, x2)) ;
+              break;
+}
