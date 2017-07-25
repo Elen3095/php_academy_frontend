b54113b1fad17ada758125e7f4186745578707ea
Elen3095 <alena3095@ukr.net>
2017-07-25 12:15:24 +0300

    clear all

--- 1.js
+++
@@ -1,9 +0,0 @@
-// Створіть метод з іменем Calculate, який прийматиме в якості параметрів три
-// цілочисельні значення і виводитиме на екран середнє арифметичне цих параметрів.
-function Calculate(x1, x2){
-  var rezult=(a+b)/2;
-  document.write (rezult);
-}
-var a =4;
-var b=6;
-Calculate(a,b);
