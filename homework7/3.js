972ff29a32ac4379e3b85f830b312d0c937dee73
Elen3095 <alena3095@ukr.net>
2017-07-29 13:40:02 +0300

    part2, objects

---
+++ homework7/3.js
@@ -0,0 +1,7 @@
+// Створити об’єкт «FE Developer» за допомогою блока ініціалізації, задати
+// властивості імені, прізвище, віку тощо.
+var FE_Developer={};
+FE_Developer['name']="Oleg";
+FE_Developer['age']=22;
+FE_Developer['surname']='bbd';
+console.log(FE_Developer);
---
+++ homework7/4.js
@@ -0,0 +1,5 @@
+var BE_Developer=ne Object();
+BE_Developer['name']="Oleg";
+BE_Developer['age']=22;
+BE_Developer['surname']='bbd';
+console.log(FE_Developer);
